2b005357024264c102619e7df9a08d4634062a8a
Kevin0918 <2473998520@qq.com>
2017-08-14 14:47:05 +0800

    courses.adminx

---
+++ apps/organization/adminx.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+# _author_ = 'Kevin'
+# _date_ = '2017/8/14 下午2:34'
+import xadmin
+
+from .models import CityDict
+from .models import CourseOrg
+from .models import Teacher
+
+
+class CityDictAdmin(object):
+    list_display = ['name', 'desc', 'add_time']
+    search_fields = ['name', 'desc']
+    list_filter = ['name', 'desc', 'add_time']
+
+
+class CourseOrgAdmin(object):
+    list_display = ['name', 'desc', 'click_nums', 'fav_nums', 'image', 'address', 'city', 'add_time']
+    search_fields = ['name', 'desc', 'click_nums', 'fav_nums', 'image', 'address', 'city']
+    list_filter = ['name', 'desc', 'click_nums', 'fav_nums', 'image', 'address', 'city', 'add_time']
+
+
+class TeacherAdmin(object):
+    list_display = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums',
+                    'add_time']
+    search_fields = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums']
+    list_filter = ['org', 'name', 'work_years', 'work_company', 'work_position', 'points', 'click_nums', 'fav_nums',
+                   'add_time']
+
+
+xadmin.site.register(CityDict, CityDictAdmin)
+xadmin.site.register(CourseOrg, CourseOrgAdmin)
+xadmin.site.register(Teacher, TeacherAdmin)
