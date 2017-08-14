5a7fd043f4065729de1eccfb4d37137cbbf27ec5
Kevin0918 <2473998520@qq.com>
2017-08-14 14:14:36 +0800

    users.adminx

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -5,10 +5,20 @@
 import xadmin
 
 from .models import EmailVerifyRecord
+from .models import Banner
 
 
 class EmailVerifyRecordAdmin(object):
-    pass
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    search_fields = ['code', 'email', 'send_type']  # 搜索字段
+    list_filter = ['code', 'email', 'send_type', 'send_time']  # 过滤器
+
+
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url', 'index', 'add_time']
+    search_fields = ['title', 'image', 'url', 'index']  # 搜索字段
+    list_filter = ['title', 'image', 'url', 'index', 'add_time']  # 过滤器
 
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(Banner, BannerAdmin)
